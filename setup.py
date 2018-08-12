8df7aaee06a2e775a69d18011c91dce2a33e017b
Isaac Schaaf <ischaaf@riotgames.com>
2018-08-11 21:07:52 -0700

    added egg info, not sure if that's the right thing to do, but lets see

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name="pymq",
+    version="0.1.0",
+    author="Isaac Schaaf",
+    author_email="zeekus99@gmail.com",
+    description=("A queue wrapper library for python"),
+    license="Apache-2.0",
+    keywords="queue message-queue",
+    packages=['pymq'],
+)
